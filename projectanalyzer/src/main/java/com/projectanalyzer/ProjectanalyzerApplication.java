40c653711276d96bd4772527f32898f188b89e19
Kleiton Ewerton <kleitonewertonoliveira@gmail.com>
2024-11-08 12:36:26 -0300

    salvando possiveis utilizações

--- projectanalyzer/src/main/java/com/projectanalyzer/ProjectanalyzerApplication.java
+++ projectanalyzer/src/main/java/com/projectanalyzer/ProjectanalyzerApplication.java
@@ -1,5 +1,9 @@
 package com.projectanalyzer;
 
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectanalyzerApplication {
 
 	public static void main(String[] args) {
+
+		try {
+
+			GitHub github = new GitHubBuilder().build();
+
+			// // Obtenha informações sobre um usuário
+			GHUser user = github.getUser("KleitonEwerton");
+			// System.out.println("Nome: " + user.getName());
+			// System.out.println("Localização: " + user.getLocation());
+
+			// // Liste os repositórios do usuário
+			// for (GHRepository repo : user.listRepositories()) {
+			// System.out.println(repo.getName());
+			// repo.listLanguages().forEach((k, v) -> {
+			// System.out.println(k + " : " + v);
+			// });
+			// }
+
+		} catch (Exception e) {
+
+		}
+
 		SpringApplication.run(ProjectanalyzerApplication.class, args);
 	}
 
